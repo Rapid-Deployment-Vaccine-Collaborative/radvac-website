b134d1f062a85d6e9459267c893c9a8b94864de7
Dan Elton <dan@radvac.org>
2026-05-16 18:47:41 -0400

    attempt to fix build issues

--- src/lib/wordpress/client.ts
+++ src/lib/wordpress/client.ts
@@ -5,10 +5,10 @@ interface GraphQLResponse<T> {
   errors?: Array<{ message: string }>;
 }
 
-const MAX_RETRIES = 6;
-const BASE_DELAY_MS = 1500;
-const MAX_CONCURRENCY = 1;
-const INTER_REQUEST_GAP_MS = 250;
+const MAX_RETRIES = 3;
+const BASE_DELAY_MS = 800;
+const MAX_CONCURRENCY = 2;
+const INTER_REQUEST_GAP_MS = 75;
 
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
